20392c61cabce1597fe92fe3472018e3497a49ec
mac <shebah712@gmail.com>
2020-10-05 08:35:47 -0700

    fix localhost issues

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 // Mongo connection
 // console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.ATLAS_URI || process.env.MONGO_URI, {
+ // process.env.ATLAS_URI || 
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
