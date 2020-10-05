301bcc65291ea0225dd23d3541d41a5f86ea0e52
mac <shebah712@gmail.com>
2020-10-05 06:05:57 -0700

    prep for heroku deployment

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Mongo connection
-console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, {
+// console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.ATLAS_URI || process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
